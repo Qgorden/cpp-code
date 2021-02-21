82eb2c6b15059243d9bbd43ca7ea4d84cd08a5ad
Qgorden <756288780@qq.com>
2021-02-21 20:18:51 +0800

    my first commit

--- BubbleSort.c
+++ BubbleSort.c
@@ -8,29 +8,29 @@ struct SListNode* BubbleSort(H * L)
 {
     H*_L=Create();
     _L->next=L;
-    int i,count = 0,num;
-    H *p,*q,*tail;
+    int i,count = 0,num;//记录链表结点的个数，num进行内部循环
+    H *p,*q,*tail;//三个指针，进行冒泡排序
     p = _L;
-    while(p->next != NULL)
+    while(p->next != NULL)//计算出结点的个数
     {
-        count++;
+        count++;//（1）
         p = p->next;
     }
-    for(i=0; i < count - 1; i++) 
+    for(i=0; i < count - 1; i++) //外层循环，跟数组冒泡类似
     {
-        num = count - i - 1;
-        q=_L->next;
-        p=q->next;
-        tail=_L;
-        while(num--)
+        num = count - i - 1;//记录内层循环的次数
+        q=_L->next;//令q指向第一个结点
+        p=q->next;//令p指向后一个结点
+        tail=_L;//让tail始终指向q前一个结点，方便交换，也方便进行下一步操作
+        while(num--)//内层虚幻 次数和数组冒泡排序一样
         {
-            if(q->_data.totalcount < p->_data.totalcount)
+            if(q->_data.totalcount < p->_data.totalcount)//该结点的值大于后一个结点，就交换
             {
                 q->next=p->next;
                 p->next=q;
                 tail->next=p;
             }
-            tail = tail->next;
+            tail = tail->next;//（2）
             q=tail->next;
             p=q->next;
         }
--- QuickSort.c
+++ QuickSort.c
@@ -29,4 +29,29 @@ int partition2(SDataType arr[],int l,int r)
     arr[j] = t;
     return j;
 }
-
+void quick_sort(SDataType arr[],int l,int r)
+{
+    if(l >= r)
+        return;
+    int p = partition2(arr,l,r);
+    quick_sort(arr,l,p-1);
+    quick_sort(arr,p+1,r);
+}
+struct SListNode* Quicksort(H * L)
+{
+    H*_L=L;
+    int n=0;
+    while(_L!=NULL)
+    {
+        A[n++]=_L->_data;
+        _L=_L->next;
+    }
+    quick_sort(A,0,n-1);
+    _L=L;
+    for(int i=0; i<n; i++)
+    {
+        _L->_data=A[i];
+        _L=_L->next;
+    }
+    return L;
+}
