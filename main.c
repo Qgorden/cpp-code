530594e719edc76a3320a564b5aa4f1602b38d5b
Qgorden <756288780@qq.com>
2021-03-06 08:21:51 +0800

    my first commit

--- main.c
+++ main.c
@@ -5,9 +5,17 @@
 #include"List.h"
 #include"fileread.h"
 #include"filewirte.h"
+#include"BubbleSort.h"
+#include"QuickSort.h"
+#include"RadixSort.h"
 int main()
 {
     H *head=read();
     write("result.txt",head);
+//    head=Quicksort(head);
+//     head=Heapsort(head);
+    head=RadixSort(head);
+//    head=BubbleSort(head);
+    write("sort.txt",head);
     return 0;
 }
